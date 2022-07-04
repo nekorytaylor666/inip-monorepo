d0e8151fdb06b28c0b106a87d9c2ce788d67056f
Hellomik2002 <Hellomik2002@gmail.com>
2022-07-04 17:34:50 +0600

    fix: model in nft statistc

--- apps/inip-admin/src/statistics/controller/nft_collection.controler.ts
+++ apps/inip-admin/src/statistics/controller/nft_collection.controler.ts
@@ -3,55 +3,64 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { ListingAdapter } from 'src/mongoose/listing_metadata.model';
 import { PopularCollectionTokenModel } from '../model/popular_collection_tokens.model';
+import { SellTokenEntity } from '../model/sell_token.model';
 
 @Controller()
 export class NftStatisticController {
   constructor(
+    @InjectModel(SellTokenEntity.name)
+    private sellTokenEntityModel: Model<SellTokenEntity>,
     @InjectModel(ListingAdapter.name)
-    private listingAdapter: Model<ListingAdapter>,
+    private listingAdapterModel: Model<ListingAdapter>,
   ) {}
 
   @Get('get_popular')
   async getPopularCollection(): Promise<PopularCollectionTokenModel[]> {
-    return this.listingAdapter.aggregate([
-      {
-        $group: {
-          _id: '$contractAddress',
-          totalSaleAmount: {
-            $sum: 1,
+    const cursor =
+      this.sellTokenEntityModel.collection.aggregate<PopularCollectionTokenModel>(
+        [
+          {
+            $group: {
+              _id: '$contractAddress',
+              totalSaleAmount: {
+                $sum: 1,
+              },
+            },
           },
-        },
-      },
-      {
-        $sort: {
-          totalSaleAmount: -1,
-        },
-      },
-      {
-        $limit: 3,
-      },
-      {
-        $lookup: {
-          from: 'nftcollectionentities',
-          localField: '_id',
-          foreignField: 'address',
-          as: 'nftCollectionMetadata',
-        },
-      },
-      {
-        $addFields: {
-          nftCollectionMetadata: {
-            $first: '$doc',
+          {
+            $sort: {
+              totalSaleAmount: -1,
+            },
           },
-        },
-      },
-    ]);
+          {
+            $limit: 3,
+          },
+          {
+            $lookup: {
+              from: 'nftcollectionentities',
+              localField: '_id',
+              foreignField: 'address',
+              as: 'nftCollectionMetadata',
+            },
+          },
+          {
+            $addFields: {
+              nftCollectionMetadata: {
+                $first: '$nftCollectionMetadata',
+              },
+            },
+          },
+        ],
+      );
+    const objs = await cursor.toArray();
+    cursor.close();
+    return objs;
   }
 
   @Get('newest_listing')
   async getNewset(): Promise<ListingAdapter[]> {
     return (
-      await this.listingAdapter.find(
+      await this.listingAdapterModel.find(
         {},
         {},
         {
