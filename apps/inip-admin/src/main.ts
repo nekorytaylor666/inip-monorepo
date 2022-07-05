fc860cf715c466b4377ea6d7ef0c5eff89e31bab
Hellomik2002 <Hellomik2002@gmail.com>
2022-07-05 17:36:44 +0600

    set 443 and 80 port

--- apps/inip-admin/src/main.ts
+++ apps/inip-admin/src/main.ts
@@ -1,13 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
+import * as express from 'express';
+import * as http from 'http';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ExpressAdapter } from '@nestjs/platform-express';
 
 export const marketPlaceAddress = '0x5B360DbE1d039B80beEF7dE29EC2B0B832964d1f';
 
 const bootstrap = async () => {
   // AdminJS.registerAdapter(AdminJSMongoose);
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const server = express();
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server), {
+    cors: true,
+  });
 
   const config = new DocumentBuilder()
     .setTitle('Inip DOC')
@@ -17,6 +23,11 @@ const bootstrap = async () => {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  await app.listen(80);
+  await app.init();
+
+  http.createServer(server).listen(80);
+  http.createServer(server).listen(443);
+  // await app.listen(80);
+  // await app.listen(443);
 };
 bootstrap();
