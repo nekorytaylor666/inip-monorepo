66ac05af3e19e139628c7839320848567264f9b1
Hellomik2002 <Hellomik2002@gmail.com>
2022-07-05 17:13:57 +0600

    build: set 80 port A

--- apps/inip-admin/src/main.ts
+++ apps/inip-admin/src/main.ts
@@ -17,6 +17,6 @@ const bootstrap = async () => {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  await app.listen(3001);
+  await app.listen(80);
 };
 bootstrap();
